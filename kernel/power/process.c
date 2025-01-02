23deabc362176dbbf205aa6215d7ef552b832f7d
Rissu J <rissu.ntk@gmail.com>
2025-01-02 13:19:48 +0000

    kernel: Import OEM Changes
    Signed-off-by: Rissu J <rissu.ntk@gmail.com>

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -26,7 +26,7 @@
 /*
  * Timeout for stopping processes
  */
-unsigned int __read_mostly freeze_timeout_msecs = 20 * MSEC_PER_SEC;
+unsigned int __read_mostly freeze_timeout_msecs = 10 * MSEC_PER_SEC;
 
 static int try_to_freeze_tasks(bool user_only)
 {
@@ -50,6 +50,11 @@ static int try_to_freeze_tasks(bool user_only)
 		todo = 0;
 		read_lock(&tasklist_lock);
 		for_each_process_thread(g, p) {
+			if (pm_wakeup_pending()) {
+				wakeup = true;
+				break;
+			}
+
 			if (p == current || !freeze_task(p))
 				continue;
 
@@ -66,7 +71,7 @@ static int try_to_freeze_tasks(bool user_only)
 		if (!todo || time_after(jiffies, end_time))
 			break;
 
-		if (pm_wakeup_pending()) {
+		if (wakeup || pm_wakeup_pending()) {
 			wakeup = true;
 			break;
 		}
@@ -111,7 +116,7 @@ static int try_to_freeze_tasks(bool user_only)
 			elapsed_msecs % 1000);
 	}
 
-	return todo ? -EBUSY : 0;
+	return (todo || wakeup) ? -EBUSY : 0;
 }
 
 /**
