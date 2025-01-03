806699b91710eedf07078524e8d79de410ea29ef
Rissu J <rissu.ntk@gmail.com>
2025-01-03 11:53:10 +0000

    block: Cherry-pick from upstream
    Signed-off-by: Rissu J <rissu.ntk@gmail.com>

--- block/elevator.c
+++ block/elevator.c
@@ -990,33 +990,26 @@ int elevator_init_mq(struct request_queue *q)
 	struct elevator_type *e;
 	int err = 0;
 
-	if (q->tag_set && q->tag_set->flags & BLK_MQ_F_NO_SCHED_BY_DEFAULT)
-		return 0;
-
 	if (q->nr_hw_queues != 1)
 		return 0;
 
-	/*
-	 * q->sysfs_lock must be held to provide mutual exclusion between
-	 * elevator_switch() and here.
-	 */
-	mutex_lock(&q->sysfs_lock);
+	WARN_ON_ONCE(test_bit(QUEUE_FLAG_REGISTERED, &q->queue_flags));
+
 	if (unlikely(q->elevator))
-		goto out_unlock;
-	if (IS_ENABLED(CONFIG_IOSCHED_BFQ)) {
-		e = elevator_get(q, "bfq", false);
-		if (!e)
-			goto out_unlock;
-	} else {
-		e = elevator_get(q, "mq-deadline", false);
-		if (!e)
-			goto out_unlock;
-	}
+		goto out;
+
+		if (IS_ENABLED(CONFIG_BFQ_DEFAULT)) {
+			e = elevator_get(q, "bfq", false);
+		} else {
+			e = elevator_get(q, "mq-deadline", false);
+		}
+
+	if (!e)
+		goto out;
 	err = blk_mq_init_sched(q, e);
 	if (err)
 		elevator_put(e);
-out_unlock:
-	mutex_unlock(&q->sysfs_lock);
+out:
 	return err;
 }
 
